87dfa18597231c9654165f175903470e6a5ec42a
Karinuy <Karinuy0@outlook.com>
2025-11-05 14:07:47 +0800

    v1.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,11 @@ import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
 
 export const siteConfig = {
-  title: 'UUZ导航 | ACG导航',
-  url: 'https://uuznav.com',
-  description: '简约美观实用的导航网站，这里收录了许多优秀优质的网站',
+  title: 'UUZ导航 | ACG导航站',
+  url: 'https://www.uuznav.com',
+  description: '一个简约美观实用的导航网站，这里收录着超100个优秀优质的网站！',
   keywords: 'UUZ导航,ACG导航,网址导航,导航网站,网站收录,常用网站,galgame网站,资源网站,漫画网站,动漫网站,轻小说网站',
-  author: 'UUZ Navigation'
+  author: 'UUZNav'
 }
 
 export default defineConfig({
