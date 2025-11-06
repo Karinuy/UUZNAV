cd589ea1e0440df2905f6c5845352fd20449006d
Karinuy <Karinuy0@outlook.com>
2025-11-06 08:46:00 +0800

    fix: 修复一些小问题~

--- src/config/advertisement.ts
+++ src/config/advertisement.ts
@@ -5,24 +5,14 @@ export const advertisementConfig = {
   // 轮播图自动播放
   autoplay: true,
   // 轮播间隔时间（毫秒）
-  interval: 5000,
+  interval: 4000,
   // 广告列表
   items: [
     {
-      image: 'https://r2.limulu.moe/2025/09/15/229f1ed5f177c4e7054fb1da2dd66c4a/1757931263-magicmao.avif',
-      link: 'https://example.com/ad1',
+      image: '/banner/',
+      link: '/',
       alt: '1'
     },
-    {
-      image: 'https://r2.limulu.moe/2025/08/19/17eca4ebe880e12f7892f04bdcb4cf33/1755574107-ai.avif',
-      link: 'https://example.com/ad2',
-      alt: '2'
-    },
-    {
-      image: 'https://r2.limulu.moe/2025/09/04/487d3be1b1f2307c97dca5a9600694d8/1756945283-afy.gif',
-      link: 'https://example.com/ad3',
-      alt: '3'
-    }
   ]
 }
 
